421efd539d42623715ccc495ba6d51b730c924fc
pabsanort2 <pabsanort2@alum.us.es>
2022-06-13 20:07:52 +0200

    T-02: Login and registration screens
    Added the logic and the templates needed for the registration and login screens. Also added the debug configuration needed for vscode.

--- GameetProject/GameetApp/views.py
+++ GameetProject/GameetApp/views.py
@@ -1,3 +1,24 @@
+import re
 from django.shortcuts import render
+from .forms import UserForm, GameetUserForm
+from django.http import HttpResponseRedirect
 
 # Create your views here.
+
+def register(request):
+    submitted = False
+    if request.method == "POST":
+        form1 = UserForm(request.POST)
+        form2 = GameetUserForm(request.POST, request.FILES)
+        if form1.is_valid() and form2.is_valid():
+            form1.save()
+            gameetUser = form2.save(commit=False)
+            gameetUser.user = form1.save()
+            gameetUser.save()
+            return HttpResponseRedirect('/register?submitted=True')
+    else:
+        form1 = UserForm()
+        form2 = GameetUserForm()
+        if 'submitted' in request.GET:
+            submitted = True
+    return render(request,'register.html',{'form1': form1, 'form2': form2,'submitted':submitted})
--- GameetProject/GameetProject/urls.py
+++ GameetProject/GameetProject/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from re import template
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth.views import LoginView
+from GameetApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', LoginView.as_view(template_name='login.html')),
+    path('register/', views.register),
 ]
